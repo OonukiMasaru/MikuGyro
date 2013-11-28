b546acdb5d1208f2b24185747f7e000abcb1c052
OonukiMasaru <oonuki_masaru@yahoo.co.jp>
2013-11-28 17:14:31 +0900

    Unity Miku Dance
    ジャイロ対応 for Android

---
+++ Assets/Script/GyroRotate.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+private var myTransform:Transform;
+
+function Start () {
+	Input.gyro.enabled = true;
+	myTransform = transform;
+}
+
+function Update () {
+	var gyro:Quaternion = Input.gyro.attitude;
+
+	// gyro.x *= -1.0;
+	// gyro.y *= -1.0;
+	myTransform.localRotation = gyro;
+}
---
+++ Assets/Script/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		//Androidで戻るキーでアプリを終了させる
+		if(Input.GetKey(KeyCode.Escape)){
+			Application.Quit();
+		}
+
+	}
+}
